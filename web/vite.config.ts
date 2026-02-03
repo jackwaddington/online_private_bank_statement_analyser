17866f2d83b28ef7ab2ac0f8911e1b949f6596e9
Jack Waddington <jack@waddington.me>
2026-02-03 02:30:58 +0200

    update workslows and vite.config.ts

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/online_private_bank_statement_analyser/',
   test: {
     globals: true,
     environment: 'jsdom',
